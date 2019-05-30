fbad4a880e444603b3aae800c924f516e314a9ba
hanhaoxuan <h1047741619@163.com>
2019-05-30 22:51:35 +0800

    a

--- Like/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import LikeResources
-# Register your models here.
-admin.site.register(LikeResources)
--- User/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from django.contrib import admin
-from . import models
-
-admin.site.register(models.NormalUser)
-admin.site.register(models.ExpertUser)
-admin.site.register(models.Administrator)
-admin.site.register(models.ConfirmString)
